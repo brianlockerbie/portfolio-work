859d38adc4c8033bece44b697979b51267390ec8
Brian <brian.lockerbie@gmail.com>
2022-09-09 18:08:45 -0700

    added description and technologies used for portfolio projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -31,14 +31,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Code Refactor
                         </span>
+                        <p>Refactored codebase to have more accessibility and better positioning.</p>
+                        <br></br>
+                        <p><b>Made with: CSS, HTML</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://brianlockerbie.github.io/code-refactor/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/code-refactor'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-md px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -54,14 +57,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Coding Quiz
                         </span>
+                        <p>A coding quiz application that demonstrates the use on Web API's.</p>
+                        <br></br>
+                        <p><b>Made with: CSS, HTML, JavaScript</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://brianlockerbie.github.io/coding-quiz'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href="https://github.com/brianlockerbie/coding-quiz">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -77,14 +83,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Note Taker
                         </span>
+                        <p>Application can be used to write & save notes. Also returns older & current notes.</p>
+                        <br></br>
+                        <p><b>Made with: Node, JavaScript, jQuery, Express</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://radiant-chamber-54860.herokuapp.com/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/note-taker-app'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -100,14 +109,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Work Day Scheduler
                         </span>
+                        <p>A calendar-style scheduler application that uses third-party API & JavaScript.</p>
+                        <br></br>
+                        <p><b>Made with: CSS, HTML, JavaScript, jQuery & third-party-api</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://brianlockerbie.github.io/work-day-scheduler'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/work-day-scheduler'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -123,14 +135,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Password Generator
                         </span>
+                        <p>Application allows you to generate a random password based on your own password criteria by using JavaScript.</p>
+                        <br></br>
+                        <p><b>Made with: CSS, HTML, JavaScript</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://brianlockerbie.github.io/password-generator-app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/password-generator-app'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -146,14 +161,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Tech Blog
                         </span>
+                        <p>Users can create posts & comment on other posts to share thoughts in the tech community.</p>
+                        
+                        <p><b>Made with: MySQL, Express, Node, MVC, Handlebars, Sequelize</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://shrouded-headland-93448.herokuapp.com'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/tech-blog'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -169,14 +187,17 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Weather Dashboard
                         </span>
+                        <p>This application uses server-side API and JavaScript to create a weather dashboard.</p>
+                        <br></br>
+                        <p><b>Made with: CSS, JavaScript, jQuery, fetch-api, server-side-api</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://brianlockerbie.github.io/weather-dashboard'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/weather-dashboard'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -192,14 +213,16 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Team Profile Generator
                         </span>
+                        <p>Using Node.js and it's modules to create a generated HTML team profile to demonstrate OOP and TDD.</p>
+                        <p><b>Made with: Node, JavaScript, Jest, OOP, Inquirer</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://drive.google.com/file/d/1HSfNE8VwWTKqD7WeNsrs2kksJ2Gye4R3/view'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/brianlockerbie/team-profile-generator'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
@@ -215,14 +238,16 @@ const Work = () => {
                         <span className='text-2xl font-bold text-white -tracking-wider'>
                           Population Density Checker
                         </span>
+                        <p>A Population Density Checker application allows the user to check the population of a desired location.</p>
+                        <p><b>Made with: CSS, HTML, JavaScript, api-requests, server-side-api, Tailwind</b></p>
                         <div className='pt-8 text-center'>
                             <a href='https://project01team10.github.io/population-density-checker/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Demo
                                 </button>
                             </a>
                             <a href='https://github.com/project01team10/population-density-checker'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>
                                   Code
                                 </button>
                             </a>
