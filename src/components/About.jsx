2bdfbf2b8d15e1f04c5b8f338ce502e285351134
Brian <brian.lockerbie@gmail.com>
2022-12-04 21:34:39 -0800

    update build for gh-pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className='flex flex-col justify-center items-center w-full h-full'>
           <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
               <div className='sm:text-right pb-8 pl-4'>
-                <p className='flex text-4xl font-bold underline #9333ea border-purple-600'>
+                <p className='text-4xl font-bold border-b-4 inline border-purple-600'>
                 About
                 </p>
               </div>
@@ -19,7 +19,7 @@ const About = () => {
                   <div>
                     <p>I am passionate about building & desgining excellent software that improves
                     the lives of those around me. I specialize in front end development.
-                    I love creating functional beautiful applications that are one of kind.</p>
+                    I love creating functional beautiful applications that are one of a kind.</p>
                   </div>
                 </div>
             </div>
